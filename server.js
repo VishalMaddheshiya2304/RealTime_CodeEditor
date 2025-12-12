894e0a94529a17bdc607333677ebb70d2102b88b
Vishal Maddheshiya <91178200+VishalMaddheshiya2304@users.noreply.github.com>
2025-12-12 11:27:06 +0530

    Allow CORS from any origin

--- server.js
+++ server.js
@@ -9,11 +9,12 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "*",
         methods: ["GET", "POST"],
         credentials: true
     }
 });
+
 
 app.use(express.static('build'));
 app.use((req, res, next) => {
